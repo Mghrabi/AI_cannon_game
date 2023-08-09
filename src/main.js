2dd6547b15048df0c4c6ac13c05f0c8c54cf5657
mghrabi <ahmedhisham12351@gmail.com>
2023-08-09 09:56:45 +0300

    small changes to folders structure

--- src/main.js
+++ src/main.js
@@ -1,18 +1,9 @@
-const setupCanvas = (can) => {
-    can.height = window.innerHeight;
-    can.width = window.innerWidth / 2;
-}
-
 canvas = document.getElementById('canvas');
 network_canvas = document.getElementById('network');
 setupCanvas(canvas)
 setupCanvas(network_canvas)
 
-
 ctx = canvas.getContext('2d')
-ctx.fillRect(10, 20, 50, 50);
 
-ctx.beginPath();
-ctx.arc(canvas.width/2, canvas.height/2, 40, 0, 2 * Math.PI)
-ctx.stroke();
-
+cannon = new Cannon();
+cannon.draw(ctx);
---
+++ src/util/util.js
@@ -0,0 +1,6 @@
+// to split the screen between the two canvases 
+const setupCanvas = (can) => {
+    can.height = window.innerHeight;
+    can.width = window.innerWidth / 2;
+}
+
